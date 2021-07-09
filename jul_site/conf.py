4c401ccf4173a0e2b3fa94c2da902994d8536da6
Julia Paranich | Qconcepts <julia.paranich@qconcepts.nl>
2021-07-09 12:40:25 +0200

    changed themee

--- jul_site/conf.py
+++ jul_site/conf.py
@@ -187,7 +187,7 @@ THEME = "bootblog4-jinja"  # bootblog4 uses mako, bootblog4-jinja uses jinja
 # A theme color. In default themes, it might be displayed by some browsers as
 # the browser UI color (eg. Chrome on Android). Other themes might also use it
 # as an accent color (the default ones don’t). Must be a HEX value.
-THEME_COLOR = '#5670d4'
+THEME_COLOR = '#33ff33'
 
 # Theme configuration. Fully theme-dependent. (translatable)
 # Samples for bootblog4 (enabled) and bootstrap4 (commented) follow.
